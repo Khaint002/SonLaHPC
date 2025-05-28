d745ecb1d5572774226dc3904d2b3744c2a43b02
Khaint002 <khainhokkazuo@gmail.com>
2025-05-28 16:34:04 +0700

    update

--- pages/History/history.js
+++ pages/History/history.js
@@ -813,7 +813,7 @@ function generatePopupValueHTML(loc) {
                 <tr><td>Mực nước: ${loc.RN ?? 0} m</td></tr>
                 <tr><td><b>Lượng mưa: ${loc.RD ?? 0} mm</b></td></tr>
                 <tr><td><b>Lưu lượng nước: ${loc.QN ?? 0} m³/s</b></td></tr>
-                <tr><td><b>Tốc độ dòng chảy: ${loc.VN ?? 0} m³/s</b></td></tr>
+                <tr><td><b>Tốc độ dòng chảy: ${loc.VN ?? 0} m/s</b></td></tr>
             `;
             break;
         default:
